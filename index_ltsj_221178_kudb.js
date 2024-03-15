83cf36a95b3db2c6efebfaf377f7febce4aefb1d
SokoliukA <sokoliukanna9@gmail.com>
2024-03-15 12:54:32 +0200

    Added code structure

---
+++ index_ltsj_221178_kudb.js
@@ -0,0 +1,25 @@
+const reverseString = str => str.split("").reverse().join("");
+const isPalindrome = str => str === str.split("").reverse().join("");
+const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
+
+const isEven = num => num % 2 === 0;
+orange + kiwi
+const removeDuplicates = array => Array.from(new Set(array));
+const removeDuplicates = array => Array.from(new Set(array));
+function addNumbers(a, b) { return a + b; }
+
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+let result = performOperation(getRandomNumber(), getRandomNumber());
+
+function addNumbers(a, b) { return a + b; }
+const getRandomElement = array => array[getRandomIndex(array)];
+
+false - 22,10,11,90,61,3,83,28,63,94,6,10,60,46,87,13,6,63,81,86,26,93,72,16,0,51,77,10,26,17,88,72,52,63,29,11,76,18,45,59,54,82,7,49,46,62,96,20,60,20,38,4,8,81,6,71,84,33,37,84,93,24,72,16,0,5,3,8,69,64,10,79,50,10,44,87,36,82,80,5,14,34,59,21,37,37,58,81,2,58,59,38,34,62,7,29
+const capitalizeString = str => str.toUpperCase();
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const calculateFactorial = n => n === 0 ? 1 : n * calculateFactorial(n - 1);
+
+orange - 7,36,55,84,99,20,58,36,77,74,22,31,24,58,39,86,21,3,51,37,84,21,74,72,70,98
+
+function addNumbers(a, b) { return a + b; }
